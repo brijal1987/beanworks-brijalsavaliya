98cea4cb19f1edfd2a6dc569cbc7e19a5750fba4
Brijal Savaliya <brijal.savaliya@gmail.com>
2019-11-22 16:29:38 +0530

    changes in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-LIST_OF_APPS="nodejs mysql-server"
+LIST_OF_APPS="nodejs npm mysql-server"
 
 sudo apt-get update  # To get the latest package lists
 sudo apt-get install -y $LIST_OF_APPS
